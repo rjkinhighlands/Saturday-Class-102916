a78e16532d5f0a39c8d49f97c7aa64ec69de00bd
Ben Cheng <bencheng00@gmail.com>
2016-10-29 11:42:04 -0400

    routing done

---
+++ restaurant/data/table-data.js
@@ -0,0 +1,12 @@
+var tableArray = [
+	{
+		customerName: 'John Gotti',
+		customerEmail: 'john@cosanostra.org',
+		phoneNumber: '222-222-2222'
+		customerID: 'teflondon',
+
+	}
+];
+
+
+module.exports = tableArray;
---
+++ restaurant/data/waitinglist-data.js
@@ -0,0 +1,12 @@
+var waitingArray = [
+	{
+		customerName: 'Satan',
+		customerEmail: 'satan@aol.com',
+		phoneNumber: '666-666-6666',
+		customerID: 'damnyoualltohell'
+	}
+];
+
+
+
+module.exports = waitingArray;
